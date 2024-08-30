910246f9c541262fdd570f3fff61e2848f741b6a
daniel-macias <josedanielmr@gmail.com>
2024-08-30 10:44:17 -0600

    removed android folder

--- android/build/assets/scripts/floatingLabel.gd
+++
@@ -1,9 +0,0 @@
-extends Label
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimationPlayer.play("fade_number")
-	$AnimationPlayer.animation_finished.connect(self.on_animation_finished)
-
-func on_animation_finished():
-	queue_free()  # Remove the label from the scene after the animation
--- android/build/assets/scripts/game.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-signal game_scene_loaded
-
-func _ready():
-	# Notify GameManager that the game scene has fully loaded
-	emit_signal("game_scene_loaded")
